1e4a1cc4d50e65ae34fd733c85ffa2050839f2d6
Aks32216 <amishsingh568@gmail.com>
2021-11-08 10:16:00 +0530

    final

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const express=require("express");
 const bodyparser=require("body-parser");
-var items=["Study"];
+let items=["Study"];
+let works=[];
 const app=express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyparser.urlencoded({extended:true}));
 
-
+app.use(express.static("./public"));
 
 app.get("/",(req,res)=>{
-    var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    var today  = new Date();
-    var day=today.toLocaleDateString("en-US", options);
-    res.render("list",{Day:day,listItem:items});
+    let options = { weekday: 'long', month: 'long', day: 'numeric' };
+    let today  = new Date();
+    let day=today.toLocaleDateString("en-US", options);
+    res.render("list",{listTitle:day,listItem:items});
 })
 
 app.post("/",(req,res)=>{
-    item=req.body.nextItem;
-    items.push(item);
-    res.redirect("/");
+    let item=req.body.nextItem;
+    if(req.body.list==="Today's")
+    {
+        works.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Today's Work",listItem:works});
+})
+
+app.post("/work",(req,res)=>{
+    let item=req.body.nextItem;
+    if(item!=="")
+        works.push(item);
+    res.redirect("/work");
 })
 
 
